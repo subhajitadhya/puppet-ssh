76a0c22dbcddfa594ee82b763414fb7f56dbf3e8
root <root@subhajitmaster.puppet.com>
2017-10-11 15:29:06 +0530

    sdasd

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,6 +1,9 @@
-class ssh::service {
- service { 'ssh':
+class ssh::service(
+   Steing $service_name = $::ssh::service_name,
+) {
+ service { 'ssh-service':
   ensure => running,
+  name   =>Sservice_name,
   enable => true,
  }
 
